b4db80feea0342ad6e5caae22b86cd681201c079
Robin Van audenhaege <rvan-aud@fu-r8-p8.s19.be>
2021-06-07 10:54:44 +0200

    chooses good algo over 11 args

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rvan-aud <rvan-aud@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 10:57:15 by rvan-aud          #+#    #+#             */
-/*   Updated: 2021/06/07 10:24:13 by rvan-aud         ###   ########.fr       */
+/*   Updated: 2021/06/07 10:49:15 by rvan-aud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ static void	selectalgo(t_stacks *stacks, int count)
 		alg4(stacks);
 	else if (count == 5 || count == 6)
 		alg5_6(stacks, count);
+	else if (count <= 11)
+		algo_small(stacks, count);
 	else
 		algo(stacks, count);
 }
